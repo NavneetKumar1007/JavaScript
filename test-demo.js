896ca8cef4c74ab0c00b38a0a9fcc61a42c56506
NavneetKumar1007 <navneet.kumar1007@gmail.com>
2022-10-23 19:35:34 +0530

    JavaScript Variable

---
+++ test-demo.js
@@ -0,0 +1,10 @@
+console.log("Navneet Kumar");
+console.log(23);
+console.log({name:'Navneet Kumar',age:23});
+console.log(true,false);
+
+
+const myName='Navneet';
+const myAge=23;
+const myIdentification={name:myName,age:myAge};
+console.log(myIdentification)
